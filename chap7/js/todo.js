de644300c44f3ebafe2ff06cb19cdfac9da608ba
pki071120 <kollagajoha@gmail.com>
2023-10-23 23:08:09 +0900

    second commit

--- chap7/js/todo.js
+++ chap7/js/todo.js
@@ -2,24 +2,48 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const Todo_Key = "todo";
+let todo = [];
 
+function saveTodo() {
+  localStorage.setItem(Todo_Key, JSON.stringify(todo));
+}
 
-function paintToDo(todo){
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(todo) {
   const li = document.createElement("li");
-  const span =document.createElement("span");
-  const button = document.createElement("button");
-  button.innerText("");
-
-  li.appendChild(span);
+  const span = document.createElement("span");
   span.innerText = todo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
-function handleTodoSubmit(event){
+function handleTodoSubmit(event) {
   event.preventDefault();
-  const inputValue=toDoInput.value;
-  toDoInput.value="";
+  const inputValue = toDoInput.value;
+  toDoInput.value = "";
+  todo.push(inputValue);
   paintToDo(inputValue);
+  saveTodo();
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
+
+function sayHello(item) {
+  console.log("Ang Gi Mo Chi", item);
+}
+
+const savedTodo = localStorage.getItem(Todo_Key);
+if (savedTodo !== null) {
+  const parsedTodo = JSON.parse(savedTodo);
+  todo = parsedTodo;
+  parsedTodo.forEach(paintToDo);
+}
